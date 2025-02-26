f26a3c3e6e967ee8155b22b6e2f25f3ec20a5ea5
inesb9 <iblanco27@students.stab.org>
2025-02-26 14:21:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ var buildingHeight = getColumn(url, 6);
 var floors = getColumn(url, 7);
 var yearOpened = getColumn(url, 8);
 
-
+//this function takes a country and displays all the buildings in that country
+//result{array}= all the buildings found in country, this is what the function is going to return
+//country{string}= the data the user is going to input 
 function countryBuildings(country){
 var result = []
 for(var i = 0; i < buildingNames.length; i++){
@@ -22,6 +24,9 @@ return result
 console.log(countryBuildings("44"))
 
 
+//this function takes a country, takes all the heigths ofthe buildings in that country and finally adds them
+//buildingCountry {string}= the data the user is going to input
+// return{number} is going to return the number corresponding to totalHeight follow it by feet
 function getTotalHeights(buildingCountry){
     var totalHeight = 0
     for (var i = 0; i < buildingNames.length; i++){
@@ -36,6 +41,9 @@ return totalHeight+" feet"
 }
 console.log(getTotalHeights("china"))
 
+//this function takes a number of floors and finds the oldest building with that number of floors
+//floor{string}= the number of floors the building is going to have, what the user is going to input  
+//return {string}= the oldest building with that number of floors
 
 function oldestBuidling(floor){
     var oldest = 100000000000000;
@@ -50,7 +58,9 @@ return list
 }
 console.log(oldestBuidling(100));
 
-
+//this function takes a height and finds the closest building to that heigth
+//height{number}= the data the user is going to input,is going to be the height of the building is goig to return
+//return{string}= the buinding name with the closest height to the height the user input
 function getClosest(height){
     var bestIndex = 0
     var closest = Math.abs(buildingHeight[0] - height)
@@ -64,7 +74,9 @@ function getClosest(height){
 }
 console.log(getClosest(800))
 
-
+// this function takes the country the user is going to input and takes the height of all the buildings in that country. finally it gets the average heigth of all the buildig of that country
+//buildingCountry{string}= the information the user is going to input, the average is going to be of this string.
+//return{number}= is going to reutrn the average of the heigths of the country you chose.
 function getAvarageHeight(buildingCountry){
     var totalHeight = 0
     var numberOfBuildings = 0
